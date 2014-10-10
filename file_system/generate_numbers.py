3f7af393bcfffcfc698e6502f96af1d41d102346
svetlio <svetlio1994@gmail.com>
2014-10-10 20:40:31 +0300

    fix

--- file_system/generate_numbers.py
+++ file_system/generate_numbers.py
@@ -6,5 +6,6 @@ def main():
     file = open(sys.argv[1], "w")
     for i in range(1, int(sys.argv[2])):
         file.write(str(randint(1, 1000))+" ")
+    file.close()
 if __name__ == '__main__':
     main()
