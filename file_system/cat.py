e858d0455ae3d6f122ce872f634c0712ff199a5b
svetlio <svetlio1994@gmail.com>
2014-10-10 21:23:39 +0300

    cat

--- file_system/cat.py
+++ file_system/cat.py
@@ -3,12 +3,10 @@ import sys
 
 
 def main():
-    for arg in sys.argv:
-        if arg != "cat.py":
-            filename = arg
-            file = open(filename, "r")
-            content = file.read()
-            print (content)
+    filename = sys.argv[1]
+    file = open(filename, "r")
+    content = file.read()
+    print (content)
 
 if __name__ == '__main__':
     main()
