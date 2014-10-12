230b0c3e41e24f804c3d3b5ef000e517850a0dda
svetlio <svetlio1994@gmail.com>
2014-10-12 21:32:46 +0300

    some fix

--- file_system/pizza.py
+++ file_system/pizza.py
@@ -1,6 +1,6 @@
 from time import time
 from datetime import datetime
-from parse import *
+#from parse import *
 
 
 def main():
@@ -12,6 +12,8 @@ def main():
     while (True):
         command = input("Enter command>").split()
         used_commands.append(command)
+        ts = time()
+        stamp = datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
         if command[0] == "take":
             if command[1] not in names:
                 orders[command[1]] = float(command[2])
@@ -22,8 +24,6 @@ def main():
             for el in orders:
                 print (el+" - ", orders[el])
         elif command[0] == "save":
-            ts = time()
-            stamp = datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
             list_orders.append(stamp)
             file = open(stamp, "w+")
             for el in orders:
@@ -33,17 +33,21 @@ def main():
             for i in range(0, len(list_orders)):
                 print ("[%d] - " % (i+1) + list_orders[i])
         elif command[0] == "load":
-            ts = time()
-            stamp = datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
             if "list" not in used_commands:
                 print("Use list command before loading")
             elif stamp not in list_orders:
                 if counter == 0:
-                    print "You have not saved the current order./n If you wish to discard it, type load <number> again."
+                    print ("You have not saved the current order./n If you wish to discard it, type load <number> again.")
                     counter = 1
                 else:
                     file = open(list_orders[command[1]])
-
+        elif command[0] == "finish":
+            if stamp not in list_orders:
+                if counter == 0:
+                    counter = 1
+                    print ("You have not saved your order./n If you wish to continue, type finish again. /n If you want to save your order, type save")
+                else:
+                    break
 
 
 if __name__ == '__main__':
