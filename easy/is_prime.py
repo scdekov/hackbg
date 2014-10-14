b934ab927d3448724c4f229a89fde52c8ac57f1e
svetlio <svetlio1994@gmail.com>
2014-10-14 19:23:13 +0300

    fix main func

--- easy/is_prime.py
+++ easy/is_prime.py
@@ -1,13 +1,17 @@
 def is_prime(n):
-	
-	if n < 2 :
-		return False
-	if n == 2 :
-		return True
-	for i in range (2,n) :
-		if n % i == 0 :
-			return False
-	return True
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
 
-print (is_prime(3))
+def main():
+    print (is_prime(3))
+
+
+if __name__ == '__main__':
+    main()
