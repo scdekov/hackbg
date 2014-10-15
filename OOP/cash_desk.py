438db5ce01a0696a1ecfdee73b7861b705a6b320
svetlio <svetlio1994@gmail.com>
2014-10-15 19:10:32 +0300

    first 2 problems

--- OOP/cash_desk.py
+++ OOP/cash_desk.py
@@ -12,30 +12,35 @@ class Cash_desk(object):
             sum += el * self.money[el]
         return sum
 
-    def rec(money_arr, sum):
-
+    def _rec(self, money_arr, summ):
+        flag = False
+        if summ == 0:
+            flag = True
+        else:
+            for i in range(summ):
+                for el in money_arr:
+                    if summ - el >= 0:
+                        money_arr.remove(el)
+                        return self._rec(money_arr, summ - el)
+        return flag
 
     def can_withdraw_money(self, summ):
-        flag = False
         money_arr = []
         for el in self.money:
             for i in range(0, self.money[el]):
                 money_arr.append(el)
         money_arr = sorted(money_arr)
         money_arr = money_arr[::-1]
-        rec(money_arr, summ, flag)
-
+        return self._rec(money_arr, summ)
 
 
 def main():
     my_cash_desk = Cash_desk()
     my_cash_desk.take_money({1: 2, 50: 1, 20: 1})
-    """print (my_cash_desk.total())  # 72
-    print (my_cash_desk.can_withdraw_money(30))"""
+    print (my_cash_desk.total())
+    print (my_cash_desk.can_withdraw_money(30))
     print (my_cash_desk.can_withdraw_money(70))
-    a = 5
-    sum(a)
-    print (a)
+
 
 if __name__ == '__main__':
     main()
---
+++ OOP/product.py
@@ -0,0 +1,61 @@
+class Product:
+    def __init__(self, name, stock_price, final_price):
+        self.name = name
+        self. stock_price = stock_price
+        self. final_price = final_price
+
+    def profit(self):
+        return self.final_price - self.stock_price
+
+
+class Laptop(Product):
+    def __init__(self, name, stock_price, final_price, discspace, RAM):
+        super().__self__(name, stock_price, final_price)
+        self.discspace = discspace
+        self.RAM = RAM
+
+
+class Smartphone(Product):
+    def __init__(self, name, stock_price, final_price, display_size, mega_pixels):
+        super().__init__(name, stock_price, final_price)
+        self.display_size = display_size
+        self.mega_pixels = mega_pixels
+
+
+class Store():
+    def __init__(self, name):
+        self.name = name
+        self.products = {}
+        self.profit = 0
+
+    def load_new_products(self, product, count):
+        self.products[product] = count
+
+    def list_products(self, product_class):
+        for el in self.products:
+            if isinstance(el, type(product_class)):
+                print (el + " - ", self.products[el])
+
+    def sell_product(self, product):
+        for el in self.products:
+            if isinstance(el, type(product)):
+                self.products[el] -= 1
+                self.profit += el.profit()
+                return True
+        return False
+
+    def total_income(self):
+        return self.profit
+
+
+def main():
+    store = Store('Laptop.bg')
+    smarthphone = Smartphone('Hack Phone', 500, 820, 7, 10)
+    store.load_new_products(smarthphone, 2)
+    print (store.sell_product(smarthphone))  # True
+    print (store.sell_product(smarthphone))  # True
+    print (store.total_income())  # 640
+
+
+if __name__ == '__main__':
+    main()
