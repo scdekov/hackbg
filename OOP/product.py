5b85444ff13feb16e6f33566d93bb80e6f04a5bf
svetlio <svetlio1994@gmail.com>
2014-10-15 20:29:39 +0300

    products

--- OOP/product.py
+++ OOP/product.py
@@ -34,11 +34,11 @@ class Store():
     def list_products(self, product_class):
         for el in self.products:
             if isinstance(el, type(product_class)):
-                print (el + " - ", self.products[el])
+                print (el.name + " - ", self.products[el])
 
     def sell_product(self, product):
         for el in self.products:
-            if isinstance(el, type(product)):
+            if isinstance(el, type(product)) and self.products[el] > 0:
                 self.products[el] -= 1
                 self.profit += el.profit()
                 return True
@@ -52,6 +52,7 @@ def main():
     store = Store('Laptop.bg')
     smarthphone = Smartphone('Hack Phone', 500, 820, 7, 10)
     store.load_new_products(smarthphone, 2)
+    store.list_products(smarthphone)
     print (store.sell_product(smarthphone))  # True
     print (store.sell_product(smarthphone))  # True
     print (store.total_income())  # 640
