a7c45c689333ecea00f1069c5bcb60c07b76d357
svetlio <svetlio1994@gmail.com>
2014-10-27 20:19:26 +0200

    fix

--- week2/MusicCrawler.py
+++ week2/MusicCrawler.py
@@ -12,14 +12,19 @@ class MusicCrawler():
     def __init__(self, path):
         playlist = Playlist("newList")
         os.chdir(path)
-        for song in glob.glob("*.mp3"):
-            song = MP3(song, ID3=EasyID3)
-            playlist.add_song(Song(song.tags["title"][0], song.tags["artist"][0], "", 0, song.info.length, song.info.bitrate), )
-
+        for songa in glob.glob("*.mp3"):
+            song = MP3(songa, ID3=EasyID3)
+            try:
+                playlist.add_song(Song(song.tags["title"][0], song.tags["artist"][0], "", 0, song.info.length, song.info.bitrate))
+            except TypeError:
+                pass
+            except KeyError:
+                pass
         print (playlist)
 
 
 def main():
+    #music = MusicCrawler("/home/svetlio/Documents/programming101/week2")
     music = MusicCrawler("/home/svetlio/Music")
 
 
